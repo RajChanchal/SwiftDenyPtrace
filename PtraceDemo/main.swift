d8a395ab685aeec8b099912c7b1949d42cf10e84
Chanchal Raj <rajchanchalkhatri@gmail.com>
2018-07-17 19:32:35 +0400

    Update main.swift
    Updated to Swift 4

--- PtraceDemo/main.swift
+++ PtraceDemo/main.swift
@@ -15,7 +15,7 @@ autoreleasepool {
         CommandLine.argc,
         UnsafeMutableRawPointer(CommandLine.unsafeArgv)
             .bindMemory(
-                to: UnsafeMutablePointer<Int8>.self,
+                to: Optional<UnsafeMutablePointer<Int8>>.self,
                 capacity: Int(CommandLine.argc)),
         nil,
         NSStringFromClass(AppDelegate.self)
